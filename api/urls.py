99f56a5fb7e4f8c51739e76e5bd9f223d342d522
iyoubee <al.ayubi2020@gmail.com>
2023-11-26 06:42:53 +0700

    Feat: Auth

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,10 @@ app_name = 'api'
 urlpatterns = [
     path('', index, name='index'),
 
+    # auth
+    path('register', register, name='register'),
+    path('admin-register', register_admin, name='register'),
+    path('login', login, name='login'),
+    path('logout', logout_user, name='logout_user'),
+    path('islogedin', get_is_logedin, name='get_is_logedin'),
 ]
--- api/views.py
+++ api/views.py
@@ -15,5 +15,95 @@ from .models import UserData, Prize, RedeemedPrize, Deposit, Withdraw
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login as auth_login, logout
 
+import random
+import string
+
 def index(request):
     return JsonResponse({ "status": 200, "message": "Halo..." }, status=200)
+
+@csrf_exempt
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username and password:
+            try:
+                acc = User.objects.create(username=username)
+                if acc:
+                    acc.set_password(password)
+                    acc.save()
+                    userdata = UserData.objects.create(user=acc)
+                    userdata.username = username
+                    uppercase_chars = string.ascii_uppercase
+                    random_token = ''.join(random.choice(uppercase_chars) for _ in range(6))
+                    userdata.token = random_token
+                    userdata.save()
+                    assign_role(acc, commonUser)
+                    return JsonResponse({ "status": 200, "message": "Successfully Register!" }, status=200)
+                else:
+                    return JsonResponse({ "status": 500, "message": "Terjadi masalah!" }, status=500)
+            except:
+                return JsonResponse({ "status": 406, "message": "Username sudah pernah digunakan." }, status=406)
+        else:
+            return JsonResponse({ "status": 400, "message": "username dan password tidak boleh kosong" }, status=400)
+    return JsonResponse({"status": 502, "message": "Method not allowed"}, status=502)
+
+@csrf_exempt
+def register_admin(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username and password:
+            try:
+                acc = User.objects.create(username=username)
+                if acc:
+                    acc.set_password(password)
+                    acc.save()
+                    userdata = UserData.objects.create(user=acc)
+                    userdata.username = username
+                    userdata.save()
+                    assign_role(acc, superUser)
+                    return JsonResponse({ "status": 200, "message": "Successfully Register!" }, status=200)
+                else:
+                    return JsonResponse({ "status": 500, "message": "Terjadi masalah!" }, status=500)
+            except:
+                return JsonResponse({ "status": 406, "message": "Username sudah pernah digunakan." }, status=406)
+        else:
+            return JsonResponse({ "status": 400, "message": "username dan password tidak boleh kosong" }, status=400)
+    return JsonResponse({"status": 502, "message": "Method not allowed"}, status=502)
+
+@csrf_exempt
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                if (has_role(user, superUser)):
+                    return JsonResponse({ "status": 200, "message": "Successfully Logged In!", "role":'admin' }, status=200)
+                else:
+                    return JsonResponse({ "status": 200, "message": "Successfully Logged In!", "role":'user' }, status=200)
+            else:
+                return JsonResponse({
+                "status": 401,
+                "message": "Failed to Login, check your email/password.",
+                }, status=401)
+        else:
+            return JsonResponse({ "status": 400, "message": "username dan password tidak boleh kosong" }, status=400)
+    return JsonResponse({"message": "Method not allowed", 'status':502}, status=502)
+
+@csrf_exempt
+def logout_user(request):
+    logout(request)
+    return JsonResponse({"message": "Berhasil logout", 'status':200}, status=200)
+
+@csrf_exempt
+def get_is_logedin(request):
+    user = request.user
+    if (has_role(user, commonUser)):
+        return JsonResponse({ "isUser": "true", "role": "user"  }, status=200)
+    elif (has_role(user, superUser)):
+        return JsonResponse({ "isUser": "true", "role": "admin" }, status=200)
+    return JsonResponse({ "message": "Unauthorized" }, status=403)

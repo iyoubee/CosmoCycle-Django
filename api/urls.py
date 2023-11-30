a58c8234e6ea0cb6f89f91aba9350ff4d19edda0
Sasha Nabila Fortuna <sasha.nabila@ui.ac.id>
2023-11-30 19:50:52 +0700

    implement use case deposit to views.py and urls.py

--- api/urls.py
+++ api/urls.py
@@ -12,4 +12,18 @@ urlpatterns = [
     path('login', login, name='login'),
     path('logout', logout_user, name='logout_user'),
     path('islogedin', get_is_logedin, name='get_is_logedin'),
+
+    # admin
+    path('admin/token', admin_get_user_by_token, name='admin_get_user_by_token'),
+    path('admin/deposit/get', admin_get_deposit, name='admin_get_deposit'),
+    path('admin/deposit/count', admin_get_deposit_count, name='admin_get_deposit_count'),
+    path('admin/deposit/add', admin_add_deposit, name='admin_add_deposit'),
+
+    # user
+    path('user/token', user_get_token_by_id, name='user_get_token_by_id'),
+    path('user/data/get', user_get_data, name='user_get_data'),
+    path('user/deposit/get', user_get_deposit, name='user_get_deposit'),
+
+    # all
+    path('islogedin', get_is_logedin, name='get_is_logedin'),
 ]
